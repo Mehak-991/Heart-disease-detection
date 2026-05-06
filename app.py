0a486bddb40658cf433bfa0d6cb74e4ce279866c
Mehakverma991 <missmehak755gmail.com>
2026-05-07 01:41:19 +0530

    fix: update host configuration to default to localhost when running locally

--- app.py
+++ app.py
@@ -94,4 +94,5 @@ def predict():
 # ---------------- Run ---------------- #
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    host = "0.0.0.0" if os.environ.get("PORT") else "127.0.0.1"
+    app.run(host=host, port=port, debug=True)
